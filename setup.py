6add8d980baf123e82fe93bfdcb38c4948608a4c
Nan Xue <xuenan199@gmail.com>
2021-08-21 17:48:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,6 @@ import os,sys
 from setuptools import setup,find_packages
 
 requirements = ["torch", "torchvision"]
-nvcc = subprocess.check_output("nvcc --version|grep cuda", shell=True)
-nvcc = nvcc.decode('utf-8')
-
-is_conda = os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
-if 'cuda_11.1' in nvcc:
-    os.system("pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html")
-
-elif 'cuda_10.2' in nvcc:
-    cuda_version = '10.2'
-else:
-    raise EnvironmentError("Please install CUDA 11.1 or 10.2 on your machine")
-
 
 if __name__ == '__main__':
 
