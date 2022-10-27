67cd71a980ef7eabf50af2216541c261f7831f2d
xn <xuenan199@gmail.com>
2022-10-27 14:56:24 +0800

    modified:   downloads.sh 	modified:   readme.md

--- downloads.sh
+++ downloads.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/cherubicXN/hawp-torchhub/releases/download/HAWPv2/hawpv2-edb9b23f.pth -P checkpoints
 wget https://github.com/cherubicXN/hawp-torchhub/releases/download/HAWPv3/hawpv3-fdc5487a.pth -P checkpoints
-
+wget https://github.com/cherubicXN/hawp-torchhub/releases/download/HAWPv3/hawpv3-imagenet-03a84.pth -P checkpoints
