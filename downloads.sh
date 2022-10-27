f80a8467b448a5758ee0a5fde1fd2aa1300e15b8
xn <xuenan199@gmail.com>
2022-10-27 14:43:03 +0800

    new file:   downloads.sh

---
+++ downloads.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/cherubicXN/hawp-torchhub/releases/download/HAWPv2/hawpv2-edb9b23f.pth -P checkpoints
+wget https://github.com/cherubicXN/hawp-torchhub/releases/download/HAWPv3/hawpv3-fdc5487a.pth -P checkpoints
+
